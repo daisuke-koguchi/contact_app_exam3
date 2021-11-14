5ff87b6fe78d8be95687daa3c091ce9afa1098f0
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-14 15:12:19 +0000

    　blog機能のdestroyアクション追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all
   end
@@ -17,15 +18,12 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログ「#{@blog.title}」を更新しました"
     else
@@ -34,10 +32,15 @@ class BlogsController < ApplicationController
   end
   
   def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログ「#{@blog.title}」を削除しました"
   end
 
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
