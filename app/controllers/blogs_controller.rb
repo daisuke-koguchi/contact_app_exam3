06719a3b61b00c2175bd11ed607c450df85bc9a7
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-14 08:47:35 +0000

    blog機能のnewアクションとcreateアクションを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,30 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+      binding.pry
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
