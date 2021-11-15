60f32d0af92e727d7035f64500fb0e96af3096f4
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-15 12:43:48 +0000

    ログイン機能sessionコントローラーcreateアクション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:notice] = 'ログインに失敗しました'
+      render :new
+    end
+
+  end
+  
 end
