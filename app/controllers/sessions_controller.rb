e1cad3cecc5a9e4b9040d6c8cf69432c4b3018ca
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-15 14:36:33 +0000

    ログイン機能sessionコントローラーdestroyアクション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    
     if user && user.authenticate(params[:session][:password])
-
+      session[:user_id] = user.id
+      redirect_to user_path(user.id), notice: "ログインに成功しました"
     else
-      flash.now[:notice] = 'ログインに失敗しました'
+      flash[:notice] = 'ログインに失敗しました'
       render :new
     end
-
   end
-  
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "ログアウトしました"
+    redirect_to new_session_path
+  end
 end
