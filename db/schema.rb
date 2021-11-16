e508a07cc242ab6a6aa3bcee27b7c2500a950556
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-16 09:09:33 +0000

    画像アップロード機能をrails g scaffoldを実行して作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_013513) do
+ActiveRecord::Schema.define(version: 2021_11_16_074240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2021_11_16_013513) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blog_id"], name: "index_favorites_on_blog_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
